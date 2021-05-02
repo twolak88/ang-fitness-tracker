f3edf9fc7bcd05a29e6fbaea5b43acff0c2b5f05
twolak88 <twolak88@gmail.com>
2021-05-02 23:31:52 +0200

    Using available exercises in new training selection

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,20 +1,23 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { TrainingService } from '../training.service';
+import { Exercise } from '../exercise.model';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
+  availableExercises: Exercise[] = [];
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) {}
 
   ngOnInit(): void {
+    this.availableExercises = this.trainingService.getAvailableExercises();
   }
 
   onStartTraining(): void {
     this.trainingStart.emit();
   }
-
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -5,11 +5,15 @@ import { Exercise } from './exercise.model';
   providedIn: 'root',
 })
 export class TrainingService {
-  availableExercises: Exercise[] = [
+  private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
   ];
-  constructor() {}
+  constructor() { }
+
+  getAvailableExercises(): Exercise[] {
+    return this.availableExercises.slice();
+  }
 }
