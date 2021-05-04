f544272eff322665d2c1f83813cae0212ea7eadb
twolak88 <twolak88@gmail.com>
2021-05-04 14:35:41 +0200

    Listening to Snapshot Changes (of Firestore, incl. Metadata)

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,6 +4,7 @@ import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -11,14 +12,22 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  availableExercises: Observable<any>;
+  availableExercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService,
               private db: AngularFirestore) { }
 
   ngOnInit(): void {
     // this.availableExercises = this.trainingService.getAvailableExercises();
-    this.availableExercises = this.db.collection('availableExercises').valueChanges();
+    this.availableExercises = this.db.collection('availableExercises').snapshotChanges()
+      .pipe(map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            ...doc.payload.doc.data() as Exercise
+          };
+        });
+      }));
   }
 
   onStartTraining(trnForm: NgForm): void {
